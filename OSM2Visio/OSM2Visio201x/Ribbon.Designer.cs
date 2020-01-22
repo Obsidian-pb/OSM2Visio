1c3275c8831a97c4bd3d59c111ceec48ec564d0e
Obsidian <obsidian-pb@mail.ru>
2020-01-22 12:38:44 +0700

    Start project

---
+++ OSM2Visio/OSM2Visio201x/Ribbon.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace OSM2Visio201x
+{
+    partial class Ribbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group = this.Factory.CreateRibbonGroup();
+            this.ImportFromOSM = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group);
+            this.tab1.Label = "OSM";
+            this.tab1.Name = "tab1";
+            // 
+            // group
+            // 
+            this.group.Items.Add(this.ImportFromOSM);
+            this.group.Label = "Данные";
+            this.group.Name = "group";
+            // 
+            // ImportFromOSM
+            // 
+            this.ImportFromOSM.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.ImportFromOSM.Image = global::OSM2Visio201x.Properties.Resources.map;
+            this.ImportFromOSM.Label = "Импортировать из OSM";
+            this.ImportFromOSM.Name = "ImportFromOSM";
+            this.ImportFromOSM.ShowImage = true;
+            // 
+            // Ribbon
+            // 
+            this.Name = "Ribbon";
+            this.RibbonType = "Microsoft.Visio.Drawing";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group.ResumeLayout(false);
+            this.group.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ImportFromOSM;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon Ribbon
+        {
+            get { return this.GetRibbon<Ribbon>(); }
+        }
+    }
+}
